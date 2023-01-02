78e15b3b087d543fa9acb396133ce24d9b791ba6
Nathan Oliveira <nathanmello2@outlook.com>
2023-01-02 17:00:25 -0300

    configured packages

---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import { Text } from "react-native"
+
+import { Container, Title } from "./styles";
+
+export default function Home() {
+    return (
+        <Container>
+            <Title>GERENCIADOR DE METAS</Title>
+        </Container>
+    );
+}
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    height: 100%;
+`;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    `;
